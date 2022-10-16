f8f87cca8c8f1607468db496e76222ad618fb54d
Tom Doder <tdoder89@gmail.com>
2022-10-16 15:35:49 -0500

    give console statements

--- variables.js
+++ variables.js
@@ -6,3 +6,8 @@ var pairsOfShoesOwned = 15
 var likesSneakers = true
 var hasACar = true
 var organized = false
+console.log(email)
+console.log(firstName)
+console.log(lastName)
+console.log(age)
+console.log(organized)
