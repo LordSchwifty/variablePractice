43e003c654e0098dafdb59822cf6c1cd21c347d7
Tom Doder <tdoder89@gmail.com>
2022-10-17 21:09:28 -0500

    change variable values

--- variables.js
+++ variables.js
@@ -1,13 +1,17 @@
 var email = "drksharlo@gmail.com"
-var firstName = "Lord"
-var lastName = "Schwifty"
+console.log(This is my email)
+var firstName = "Tom"
+var lastName = "Doder"
 var age = 33
 var pairsOfShoesOwned = 15
 var likesSneakers = true
 var hasACar = true
 var organized = false
-console.log(email)
+firstName = George
+likesSneakers = false
+pairsOfShoesOwned = 16
+console.log("be cleaner")
 console.log(firstName)
 console.log(lastName)
 console.log(age)
-console.log(organized)
+
